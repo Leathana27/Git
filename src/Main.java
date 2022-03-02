194bfe74c5db6f718c61b9edc7338ef445ddd244
Anita <hihcia580@gmail.com>
2022-03-02 16:42:55 +0100

    nowa metoda policz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+
+    static public int Policz(int a, int b) {
+        return a+b;
+    }
+
+
     public static void main(String[] args) {
-        
+        System.out.println("W pierwszym branchu; 2+3=" + Policz(2,3));
     }
 }
